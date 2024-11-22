b1974e0d2ac5336ec29a34b5911cc58e2c7fcf51
yeonwooyou <onyou2123co@gmail.com>
2024-11-22 18:59:59 +0900

    11/22 (금) login_start 화면 need : 로고 + 창 넘어가기 버튼 클릭 시 색 변환 코드

---
+++ app/src/main/java/com/example/alwaysyoung2024_fe/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.alwaysyoung2024_fe;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login_button1 = findViewById(R.id.login_alone_button);
+        Button login_button2 = findViewById(R.id.login_together_button);
+
+
+    }
+}
